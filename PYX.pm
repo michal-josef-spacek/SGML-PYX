5001540fb8cd03e918198b9e93b31f3543f5acb7
Michal Špaček <m.spacek@ibsmm.com>
2016-04-20 17:01:50 +0200

    Prepare for 0.02 version.

--- PYX.pm
+++ PYX.pm
@@ -13,7 +13,7 @@ use PYX qw(comment end_element char instruction start_element);
 use PYX::Utils qw(decode entity_decode);
 
 # Version.
-our $VERSION = 0.01;
+our $VERSION = 0.02;
 
 # Constructor.
 sub new {
@@ -275,6 +275,6 @@ L<http://skim.cz>
 
 =head1 VERSION
 
-0.01
+0.02
 
 =cut
