198d64c3cc3e35223aa0b65ae8df7a57c5a3e42d
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2021-01-27 16:07:55 +0100

    Prepare for 0.08 version

--- PYX.pm
+++ PYX.pm
@@ -10,7 +10,7 @@ use Tag::Reader::Perl;
 use PYX qw(comment end_element char instruction start_element);
 use PYX::Utils qw(decode entity_decode);
 
-our $VERSION = 0.07;
+our $VERSION = 0.08;
 
 # Constructor.
 sub new {
@@ -282,6 +282,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.07
+0.08
 
 =cut
